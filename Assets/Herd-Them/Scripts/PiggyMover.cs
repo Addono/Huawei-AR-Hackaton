e30f112c00f88264bef9ab03683f1041741b402d
Zoltan Tudlik <zoltan@smartly.io>
2018-12-16 00:22:37 +0200

    add piggy mover

---
+++ Assets/Herd-Them/Scripts/PiggyMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class PiggyMover : MonoBehaviour
+    {
+        public float Speed;
+        
+        private void Start()
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * Speed;
+        }
+    }
+}
