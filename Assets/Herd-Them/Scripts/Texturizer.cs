08e4f06ca5186eaec70648612577310e739dccdb
Zoltan Tudlik <zoltan@smartly.io>
2018-12-15 16:33:37 +0200

    add environment

---
+++ Assets/Herd-Them/Scripts/Texturizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class Texturizer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
