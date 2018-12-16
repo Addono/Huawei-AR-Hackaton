48c51003b41e41f0df6b70118620c3d6c1e16246
Zoltan Tudlik <zoltan@smartly.io>
2018-12-16 12:10:22 +0200

    cleverer piggy moving

--- Assets/Herd-Them/Scripts/PiggyMover.cs
+++ Assets/Herd-Them/Scripts/PiggyMover.cs
@@ -14,14 +14,18 @@ namespace Scripts
         [SerializeField] public List<Waypoint> PatrolPoints;
 
         private NavMeshAgent _navMeshAgent;
+        private Rigidbody _rigidbody;
         private int _currentRandomIndex;
         private bool _travelling;
         private bool _waiting;
         private float _waitTimer;
+        private bool _isStart;
 
         private void Start()
         {
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _rigidbody = GetComponent<Rigidbody>();
+            _isStart = true;
             if (PatrolPoints != null && PatrolPoints.Count >= 2)
             {
                 ChangePatrolPoint();
@@ -35,38 +39,54 @@ namespace Scripts
 
         private void FixedUpdate()
         {
-            if(_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+            if (!IsOnMesh() && !_isStart)
             {
-                _travelling = false;
-                if (EnablePatrolWaiting)
-                {
-                    _waiting = true;
-                    _waitTimer = 0f;
-                }
-                else
-                {
-                    ChangePatrolPoint();
-                    SetDestination();
-                }
+                _navMeshAgent.enabled = false;
+                _rigidbody.isKinematic = false;
+                _rigidbody.useGravity = true;
+                Debug.Log("NOT on ground");
             }
-            
-            if(_waiting)
+            else
             {
-                _waitTimer += Time.fixedDeltaTime;
-                if (_waitTimer >= TotalWaitTime)
+                _isStart = false;
+                _navMeshAgent.enabled = true;
+                _rigidbody.isKinematic = true;
+                _rigidbody.useGravity = false;
+                Debug.Log("On ground");
+                if (_travelling && _navMeshAgent.remainingDistance <= 1.0f)
                 {
-                    _waiting = false;
-                    ChangePatrolPoint();
-                    SetDestination();
+                    _travelling = false;
+                    if (EnablePatrolWaiting)
+                    {
+                        _waiting = true;
+                        _waitTimer = 0f;
+                    }
+                    else
+                    {
+                        ChangePatrolPoint();
+                        SetDestination();
+                    }
+                }
+
+                if (_waiting)
+                {
+                    _waitTimer += Time.fixedDeltaTime;
+                    if (_waitTimer >= TotalWaitTime)
+                    {
+                        _waiting = false;
+                        ChangePatrolPoint();
+                        SetDestination();
+                    }
                 }
             }
         }
+
 
         private void ChangePatrolPoint()
         {
             _currentRandomIndex = Random.Range(0, PatrolPoints.Count);
         }
-        
+
         private void SetDestination()
         {
             Vector3 targetVector = PatrolPoints[_currentRandomIndex].transform.position;
@@ -74,52 +94,12 @@ namespace Scripts
             _travelling = true;
         }
 
-//        private Vector3 _target;
-//        private float _timeToChangeDirection = 2;
-//
-//        private NavMeshAgent _navMeshAgent;
-//
-//        private void Start()
-//        {
-//            GameObject gameArea = GameObject.FindWithTag("GameArea");
-//            MeshCollider gameAreaMeshCollider = gameArea.GetComponent<MeshCollider>();
-//            _navMeshAgent = GetComponent<NavMeshAgent>();
-//            _target = GetNewTarget(gameAreaMeshCollider);
-////            _timeToChangeDirection = WaitBeforeDirectionChange;
-//            _navMeshAgent.SetDestination(_target);
-//        }
-//
-//        // Update is called once per frame
-//        private void FixedUpdate()
-//        {
-//            if (IsCloseEnough())
-//            {
-//                if (_timeToChangeDirection > 0)
-//                {
-//                    Debug.Log(_timeToChangeDirection);
-//                    _timeToChangeDirection -= Time.deltaTime;
-//                    return;
-//                }
-//
-//                GameObject gameArea = GameObject.FindWithTag("GameArea");
-//                MeshCollider gameAreaMeshCollider = gameArea.GetComponent<MeshCollider>();
-//                _target = GetNewTarget(gameAreaMeshCollider);
-//                _timeToChangeDirection = 2;
-//                _navMeshAgent.SetDestination(_target);
-//            }
-//        }
-//
-//        private Vector3 GetNewTarget(MeshCollider gameAreaMeshCollider)
-//        {
-//            Vector3 min = gameAreaMeshCollider.bounds.min;
-//            Vector3 max = gameAreaMeshCollider.bounds.max;
-//            return new Vector3(Random.Range(min.x, max.x), max.y + 0.1f, Random.Range(min.z, max.z));
-//        }
-//
-//        private bool IsCloseEnough()
-//        {
-//            if (_navMeshAgent.pathPending || _navMeshAgent.remainingDistance > _navMeshAgent.stoppingDistance) return false;
-//            return !_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude <= 0.1f;
-//        }
+        private bool IsOnMesh()
+        {
+            NavMeshHit hit;
+            bool sampleResult =
+                NavMesh.SamplePosition(transform.position, out hit, _navMeshAgent.radius, NavMesh.AllAreas);
+            return sampleResult;
+        }
     }
 }
