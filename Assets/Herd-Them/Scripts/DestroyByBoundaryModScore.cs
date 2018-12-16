e653c50ffea2eeeb75e0957d7c87d15864b648b4
Zoltan Tudlik <zoltan@smartly.io>
2018-12-16 15:48:40 +0200

    implement score text stuff

---
+++ Assets/Herd-Them/Scripts/DestroyByBoundaryModScore.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class DestroyByBoundaryModScore : MonoBehaviour
+    {
+
+        private HerdThemController _herdThemController;
+
+        private void Start()
+        {
+            GameObject world = GameObject.FindWithTag("GameController");
+            _herdThemController = world.GetComponent<HerdThemController>();
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("GoodPig"))
+            {
+                _herdThemController.DecreaseScore();
+                Destroy(other.gameObject);
+            }
+
+            if (other.gameObject.CompareTag("Enemy"))
+            {
+                _herdThemController.IncreaseScore();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
